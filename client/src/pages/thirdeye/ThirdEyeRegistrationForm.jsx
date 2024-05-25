c18cc6decbf6563e2219234cfc610cc177c72cce
shivagyawali <shiv.alfa8@gmail.com>
2024-05-25 22:55:51 +0545

    inner healing form

--- client/src/pages/thirdeye/ThirdEyeRegistrationForm.jsx
+++ client/src/pages/thirdeye/ThirdEyeRegistrationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useRef } from "react";
+import React, {useEffect, useState,useRef } from "react";
 import { jsPDF } from "jspdf";
 import html2canvas from "html2canvas";
 import './thirdEyeForm.css'
@@ -14,7 +14,7 @@ const ThirdEyeRegistrationForm = () => {
   const [previewUrl, setPreviewUrl] = useState(null);
    const currentDate = new Date();
    const shortDate = formatShortDate(currentDate);
-
+  const [isFormComplete, setIsFormComplete] = useState(false);
  const [formData, setFormData] = useState({
    fullName: "",
    dob: "",
@@ -45,7 +45,7 @@ const ThirdEyeRegistrationForm = () => {
    }
  };
   const divRef = useRef(null);
-
+ 
   const generatePdf = async () => {
     try {
       const input = divRef.current;
@@ -115,13 +115,22 @@ const ThirdEyeRegistrationForm = () => {
       for (let key in formData) {
         formDataToSend.append(key, formData[key]);
       }
+       setIsFormComplete(false);
       await sendEmailWithRegistration(formDataToSend);
+       setIsFormComplete(true);
+
     } catch (error) {
       console.error("Error uploading file:", error);
     }
   };
 
-
+ useEffect(() => {
+   const { problem, ...formFieldsWithoutProblem } = formData;
+   const isComplete = Object.values(formFieldsWithoutProblem).every(
+     (value) => value.trim() !== "" || null 
+   );
+   setIsFormComplete(isComplete);
+ }, [formData]);
 
 
 
@@ -392,10 +401,38 @@ const ThirdEyeRegistrationForm = () => {
 
       <div className="container mx-auto px-48">
         <button
-          className="bg-brand hover:bg-brand text-white font-bold py-2 px-4 rounded  col-span-3 w-40"
+          className={`${
+            isFormComplete == true
+              ? "bg-brand hover:bg-brand text-white"
+              : "bg-gray-600 hover:bg-gray-500 text-white"
+          }  font-bold py-2 px-4 rounded  col-span-3 w-40`}
           onClick={handleSubmit}
+          disabled={!isFormComplete}
         >
-          Submit
+          {!isFormComplete == true ? (
+            <>
+              <svg
+                aria-hidden="true"
+                role="status"
+                class="inline mr-3 w-4 h-4 text-white animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                ></path>
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              Filling ...
+            </>
+          ) : (
+            "Submit"
+          )}
         </button>
       </div>
     </div>
